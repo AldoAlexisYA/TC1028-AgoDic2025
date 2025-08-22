d8a84daca9bc4d8ba1dfd93cee9f6de684605e3b
AldoAlexisYA <a01825644@tec.mx>
2025-08-22 09:11:43 -0600

    Update ejercicio_4.py
    Aldo Alexis Yáñez Andreu

--- src/ejercicios/clase_4/ejercicio_4.py
+++ src/ejercicios/clase_4/ejercicio_4.py
@@ -4,3 +4,11 @@ Si el número es menor a 0 deberá imprimir negativo
 Si es igual a 0 deberá imprimir cero
 Si es mayor a 0 deberá imprimir positivo
 """
+# Aldo Alexis Yáñez Andreu
+x = float( input("Ingresa un número"))
+if x < 0:
+    print("Negativo")
+elif x == 0:
+    print("Cero")
+else:
+    print("positivo")
